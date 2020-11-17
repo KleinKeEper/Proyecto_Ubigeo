d37b7a74103adedfbafde76fa81e93291a1df16e
Christian Quinteros Alvarado <chrisdigi85@gmail.com>
2020-11-17 09:35:26 -0500

    Avnce 17112020

--- src/modelo/Distrito.java
+++ src/modelo/Distrito.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author CHRISTIAN
- */
+
 public class Distrito {
+    private int iddistrito;
+    private String nombre_distrito;
+    private String poblacion;
+
+    public Distrito() {
+    }
+
+    public Distrito(int iddistrito, String nombre_distrito, String poblacion) {
+        this.iddistrito = iddistrito;
+        this.nombre_distrito = nombre_distrito;
+        this.poblacion = poblacion;
+    }
+
+    public String getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public int getIddistrito() {
+        return iddistrito;
+    }
+
+    public void setIddistrito(int iddistrito) {
+        this.iddistrito = iddistrito;
+    }
+
+    public String getNombre_distrito() {
+        return nombre_distrito;
+    }
+
+    public void setNombre_distrito(String nombre_distrito) {
+        this.nombre_distrito = nombre_distrito;
+    }
+    
     
 }
